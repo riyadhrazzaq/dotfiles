840184b90c5a8d63525e472d7446ada31c7a4690
riyadhrazzaq <riyadhrazzaq@gmail.com>
2021-12-05 14:09:35 +0600

    rofi window alt-tab script

--- rofi/rofi-alt-tab.sh
+++ rofi/rofi-alt-tab.sh
@@ -3,7 +3,7 @@ rofi \
     -show window  \
     -kb-cancel "Alt+Escape,Escape" \
     -kb-accept-entry "!Alt-Tab,Return"\
-    -kb-row-down "Alt+Tab,Alt+Down" \
-    -kb-row-up "Alt+Shift+Tab,Alt+Up"&
+    -kb-row-down "Alt+Tab,Alt+Down,Down" \
+    -kb-row-up "Alt+Shift+Tab,Alt+Up,Up"&
 xdotool keyup Tab
 xdotool keydown Tab
